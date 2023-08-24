2f56020e00ce317c65b93b3c5a507919aa5f3892
fireMan-34 <524915379@qq.com>
2023-08-25 00:36:59 +0800

    chore:  gen:class 命令完善

--- script/createClass.ps1
+++ script/createClass.ps1
@@ -2,11 +2,16 @@
 
 $Class = Read-Host "请输入要创建的课程的节数"
 
+# 复制初始模板
+
 Get-Location | Write-Output
 
 Copy-Item -Path .\template\simple.ts -Destination .\
 
 Rename-Item -Path .\simple.ts -NewName "$Class.ts"
 
-# * 后续添加 package 自动化生成 修改 json 可能 js 脚本相对容易一些
-# * 并启动自动化调试
+# * 添加 package 自动化生成 修改 json 可能 js 脚本相对容易一些
+.\node_modules\.bin\ts-node-cwd.ps1 -r tsconfig-paths/register .\script\createClass.ts $Class
+
+# * 启动自动化调试
+pnpm run "watch:$Class"
---
+++ script/createClass.ts
@@ -0,0 +1,47 @@
+import { cwd, argv } from 'process';
+import { writeFile, readFile } from 'fs/promises';
+import packageJson from '../package.json';
+import { createConsole } from 'utils/log';
+import { join } from 'path';
+
+
+
+const logger = createConsole({
+    prefix: 'creatClass',
+    shuffix: '--------------------------------',
+});
+
+const [ nodePath, scriptPath, className ] = argv;
+
+async function genCmd () {
+    const cmdName = `watch:${className}`;
+    if (packageJson.scripts[cmdName]) {
+        logger.warn(`当前命令已存在，跳过执行 ${cmdName}`);
+    }
+    packageJson.scripts[cmdName] = `ts-node-dev -r tsconfig-paths/register --clear --respawn ${className}.ts`;
+    const nextPackageJsonContent = JSON.stringify(packageJson, null, 2);
+    await writeFile(join(cwd(), '../package.json'), nextPackageJsonContent, { encoding: 'utf8' });
+};
+
+async function updateCode() {
+    const codePath = join(cwd() , `${className}.ts`);
+    let code = await readFile(codePath, { encoding: 'utf8' });
+
+    code = code.replace(/shuffix: '',/gm, `shuffix: '${className}',`);
+
+    await writeFile(codePath, code, { encoding: 'utf-8' });
+};
+
+async function main() {
+    try {
+        if (Number.isNaN(Number(className))) {
+            throw new TypeError(`${className} must be a number`);
+        }
+        await genCmd();
+        await updateCode();
+    } catch(err) {
+        logger.error(err);
+    }
+}
+
+main();
