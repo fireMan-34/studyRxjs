6ce111591048dc03ba1763b3a9ac8bb23f0995f5
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-25 13:50:40 +0800

    ✨ feat: 修复获取发布订阅变量无法获取问题

--- utils/rxHelper.ts
+++ utils/rxHelper.ts
@@ -1,29 +1,32 @@
 import { Observable, Subscriber } from 'rxjs';
+import { createRefProxy } from 'utils/fun';
 
 /**
  * @name 极简订阅创建对象
  * @returns 
  */
 export const creatObservableAndSubscribe = <T>() => {
-  let subscribe: Subscriber<T>;
+  let subscribe: { ref: Subscriber<T> } = { ref: null };
   let _unsubscribeRef: { ref: () => void } = {
     ref: () => {},
   };
   const observable = new Observable<T>((subscriber => { 
-    subscribe = subscriber;
+    subscribe.ref = subscriber;
     return () => _unsubscribeRef.ref();
    }));
    return {
     /** 
+     * 可观察对象
      * @name 可观察对象
      * @description 可以添加观察者
      */
     observable,
     /** 
+     * 发布订阅对象
      * @name 发布订阅对象
      * @description 可以发布订阅事件
      */
-    subscribe,
+    subscribe: createRefProxy(subscribe),
     /** 设置资源释放器 */
     setUnsubscribe: (callback: () => void | Function) => {
       _unsubscribeRef.ref = callback;
