4b22ab973286a167345269e273dabd4b4c202e6b
maxufeng <unf-apqb330d4@dingtalk.com>
2023-08-25 11:19:25 +0800

    ✨ feat: ref 代理函数

--- utils/fun.ts
+++ utils/fun.ts
@@ -3,3 +3,18 @@ export function sleep (time: number = 1000) {
     setTimeout(resolve, time);
   });
 };
+
+/** @name ref 代理访问 */
+export function createRefProxy<T extends { ref: unknown }> (target: T) {
+  type IRefT = T & T['ref'];
+  return new Proxy<IRefT>(target, {
+    get(target, p, receiver) {
+      if (p !== 'ref') {
+        if (target.ref[p]) {
+          return Reflect.get(target.ref as object, p, receiver);
+        }
+      }
+      return Reflect.get(target, p, receiver);
+    },
+  });
+};
